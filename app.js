b3d19049b8651e08593a2a3233a50064c3a8ec52
may huang <mayno1024@gmail.com>
2023-02-16 09:35:00 +0800

    feat:set view engine

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const app = express()
+const exphbs = require('express-handlebars') 
 
 // set server
 const port = 3000
@@ -7,7 +8,11 @@ app.listen(port, () => {
   console.log(`the appliecation is running on port ${port}`)
 })
 
+// set veiw engine
+app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
+app.set('view engine', 'hbs')
+
 // routing for testing
 app.get('/', (req, res) => {
-  res.send('success')
+  res.render('index')
 })

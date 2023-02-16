97de58f8175a4260cd5d0a346b34aedba1c993f7
may huang <mayno1024@gmail.com>
2023-02-16 22:25:34 +0800

    feat: add real totalAmount in index page

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -6,6 +6,7 @@ const Expense = require('../../models/expense')
 router.get('/', async(req, res) => {
   let categories = await Category.find().lean()
   let allExpense = {}
+  let totalAmount = 0
 
   Expense.find()
     .lean()
@@ -13,6 +14,7 @@ router.get('/', async(req, res) => {
       for (let i = 0; i < expense.length; i++) {
         // 把每一筆expense的紀錄抓出來修改date format
         expense[i].date = expense[i].date.toISOString().slice(0, 10).replace(/-/g,'/')
+        totalAmount += expense[i].amount
         for(let x = 0; x < categories.length; x++) {
           // 讓type由object轉為string(?)
           if (`${expense[i].categoryId}` === `${categories[x]._id}`) {
@@ -22,7 +24,7 @@ router.get('/', async(req, res) => {
       }
       return allExpense = expense
     })
-    .then(() => {res.render('index', { expense: allExpense, categories })}
+    .then(() => {res.render('index', { expense: allExpense, categories, totalAmount })}
 )
     .catch(err => console.log(err))
 })

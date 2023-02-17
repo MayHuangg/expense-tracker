87df81f4818f03f600b9731d80b024ef0447ffc6
may huang <mayno1024@gmail.com>
2023-02-17 16:31:20 +0800

    feat: set login page and router

--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,10 @@ const router = express.Router()
 
 const expense = require('./modules/expense')
 const home = require('./modules/home')
+const user = require('./modules/user')
 
 router.use('/expense', expense)
+router.use('/user', user)
 router.use('/', home)
 
 module.exports = router

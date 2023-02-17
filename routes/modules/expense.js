3fabc6aacb111a0ca4e9efab0fc918418b26ccbc
may huang <mayno1024@gmail.com>
2023-02-17 11:39:31 +0800

    feat: add edit page

--- routes/modules/expense.js
+++ routes/modules/expense.js
@@ -3,18 +3,40 @@ const router = express.Router()
 const Category = require('../../models/category')
 const Expense = require('../../models/expense')
 
+// 進入新增支出的頁面
 router.get('/new', (req, res) => {
   Category.find()
     .lean()
     .then(categories => res.render('new', { categories }))
     .catch(err => console.log(err))
 })
-
+// 將使用者輸入的新增支出資料傳來後端
 router.post('/', (req, res) => {
   const { name, date, category, amount } = req.body
   Expense.create({ name, date, categoryId:category, amount })
     .then(() => res.redirect('/'))
     .catch(err => console.log(err))
+})
+// 進入修改資料的頁面
+router.get('/:id/edit', async(req, res) => {
+  const _id = req.params.id
+  const categories = await Category.find().lean()
+  let expense = {}
+  Expense.findById(_id)
+    .lean()
+    .then((data) => {
+      // 從category的db抓出種類名稱，給抓出的資料另建屬性並放入值
+      categories.map(category => {
+        if (`${category._id}` === `${data.categoryId}`) {
+          data.category = category.name
+        }
+      })
+      // 修改日期的格式，好讓它能成為預設值顯示出來(需變成2022-09-09這種格式)
+      data.date = data.date.toISOString().slice(0, 10)
+
+      return expense = data
+    })
+    .then(() => res.render('edit', { expense, categories }))
 })
 
 module.exports = router

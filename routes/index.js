54bac03a853ebb630720de492a792bbfa03e495e
may huang <mayno1024@gmail.com>
2023-02-18 10:32:57 +0800

    feat: add user authentication in routers

--- routes/index.js
+++ routes/index.js
@@ -4,9 +4,12 @@ const router = express.Router()
 const expense = require('./modules/expense')
 const home = require('./modules/home')
 const user = require('./modules/users')
+// 用物件解構賦值把auth.js中物件裡的authenticator函式拿出來，並命名為authenticator
+const { authenticator } = require('../middleware/auth')
 
-router.use('/expense', expense)
+// 放入下面當callback，讓它們在進入各router後invoke，確定認證通過後方能進入下個callback，以使用服務
+router.use('/expense', authenticator, expense)
 router.use('/users', user)
-router.use('/', home)
+router.use('/', authenticator, home)
 
 module.exports = router
